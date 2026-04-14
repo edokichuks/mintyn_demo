6eb210a8cebf3f9a141575fb3ef23a89faf46aa0
chuksDev <edokichuks@gmail.com>
2026-04-14 11:56:48 +0100

    demo project init🎉

---
+++ mintyn_app/mintyn_app/mintyn_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  mintyn_appApp.swift
+//  mintyn_app
+//
+//  Created by USER on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct mintyn_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
